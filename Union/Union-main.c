f33899c39a0d27d30612a8c1b8452be0e76bb408
deviltt <tt416926063@qq.com>
2018-03-20 22:50:39 +0800

    A = A U B

---
+++ Union/Union-main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include "Union.c"
+
+void PrintElem(LElemType_Sq e){
+	printf("%d ", e);
+}
+
+int main(int argc, char **argv)
+{
+	SqList La, Lb;
+	LElemType a[5] = {5, 2, 1, 3, 9};
+	LElemType b[7] = {7, 2, 6, 9, 11, 3, 10};
+	int i;
+
+	InitList_Sq(&La);
+	for(i = 1; i <= 5; i++)
+		ListInsert_Sq(&La, i, a[i-1];
+	
+	InitList_Sq(&Lb);
+	for(i = 1; i <= 5; i++)
+		ListInsert_Sq(&Lb, i, b[i-1];
+
+	printf("La = ");
+	ListTraverse_Sq(La, PrintElem);
+	printf("\n");
+	printf("Lb = ");
+	ListTraverse_Sq(Lb, PrintElem);
+	printf("\n\n");
+
+	printf("La = La U Lb = ");
+	Union(&La, Lb);
+	ListTraverse_Sq(La, PrintElem);
+	printf("\n\n");
+
+	return 0;
+}
